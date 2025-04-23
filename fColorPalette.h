f662337c0a023fcfbe6746aea5bfc5f7e7d75ba2
Felix Touchte Codjo <felixtouchtecodjo@gmail.com>
2025-04-23 02:56:45 -0400

    Add kBird color palette from ROOT project

--- fColorPalette.h
+++ fColorPalette.h
@@ -28,7 +28,21 @@ inline const std::vector<fColor> PALETTE1 = {
 };
 
 
+// kBird from CERN ROOT Project
 inline const std::vector<fColor> PALETTE2 = {
+	// {r, g, b}
+	{0.2082, 0.1664, 0.5293},
+	{0.0592, 0.3599, 0.8684},
+	{0.0780, 0.5041, 0.8385},
+	{0.0232, 0.6419, 0.7914},
+	{0.1802, 0.7178, 0.6425},
+	{0.5301, 0.7492, 0.4662},
+	{0.8186, 0.7328, 0.3499},
+	{0.9956, 0.7862, 0.1968},
+	{0.9764, 0.9832, 0.0539}
+};
+
+inline const std::vector<fColor> PALETTE3 = {
 	// {r, g, b}
 	{0.0, 0.0, 1.0}, // blue
 	{0.0, 1.0, 1.0}, // blue-sky
@@ -36,6 +50,7 @@ inline const std::vector<fColor> PALETTE2 = {
 	{1.0, 1.0, 0.0}, // yellow
 	{1.0, 0.0, 0.0} // red
 };
+
 
 class fColorPalette {
 	int ndiv; ///< number of divisions
